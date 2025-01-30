41523d1204dd5c0df6fa03c12432a7f827b43900
Mehdi Jeyrani <mehdijeyrani@gmail.com>
2025-01-30 13:07:46 +0330

    Add example of using filter() as a higher-order function

--- higher-order-functions.js
+++ higher-order-functions.js
@@ -43,3 +43,9 @@
   const doubledNumbers = numbers.map((num) => num * 2);
   console.log(doubledNumbers); // Output: [ 2, 4, 6, 8 ]
 }
+
+{
+  const numbers = [1, 2, 3, 4, 5, 6];
+  const evenNumbers = numbers.filter((num) => num % 2 === 0);
+  console.log(evenNumbers); // Output: [ 2, 4, 6 ]
+}
