42a04d3e670d79c2013ade924ce560f15f5d6888
Mehdi Jeyrani <mehdijeyrani@gmail.com>
2025-01-30 13:05:52 +0330

    Add example of using map() as a higher-order function

--- higher-order-functions.js
+++ higher-order-functions.js
@@ -37,3 +37,9 @@
 
   console.log(doubleAndSquare(3)); // Output: 36
 }
+
+{
+  const numbers = [1, 2, 3, 4];
+  const doubledNumbers = numbers.map((num) => num * 2);
+  console.log(doubledNumbers); // Output: [ 2, 4, 6, 8 ]
+}
