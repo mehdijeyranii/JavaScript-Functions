e9a6a3a13e3ed2f1046108341d8593ebf70b7365
Mehdi Jeyrani <mehdijeyrani@gmail.com>
2025-01-30 11:05:19 +0330

    Add function to check if a number is even or odd

--- basic-function.js
+++ basic-function.js
@@ -13,3 +13,12 @@
 
   console.log(add(5, 3));
 }
+
+{
+  function isEven(number) {
+    return number % 2 === 0;
+  }
+
+  console.log(isEven(10)); // Output: true
+  console.log(isEven(7)); // Output: false
+}
