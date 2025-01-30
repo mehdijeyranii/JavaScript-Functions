393bf88baae1b82ecfd180d60f4b40c1d9a0dcf9
Mehdi Jeyrani <mehdijeyrani@gmail.com>
2025-01-30 12:57:58 +0330

    Add higher-order function example using arrow function

--- higher-order-functions.js
+++ higher-order-functions.js
@@ -9,3 +9,11 @@
 
   console.log(operateOnNumbers(3, 4, add)); // Output: 7
 }
+
+{
+  const multiply = (a, b) => a * b;
+
+  const calculate = (a, b, operation) => operation(a, b);
+
+  console.log(calculate(5, 6, multiply)); // Output: 30
+}
