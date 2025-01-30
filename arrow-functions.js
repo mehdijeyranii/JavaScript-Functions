55afaa3d335983b1e1e7d2881db132437e5adead
Mehdi Jeyrani <mehdijeyrani@gmail.com>
2025-01-30 11:41:20 +0330

    Add multi-line arrow function example

--- arrow-functions.js
+++ arrow-functions.js
@@ -15,3 +15,12 @@
   const add = (a, b) => a + b;
   console.log(add(3, 7)); // Output: 10
 }
+
+{
+  const multiply = (a, b) => {
+    let result = a * b;
+    return result;
+  };
+
+  console.log(multiply(4, 5)); // Output: 20
+}
