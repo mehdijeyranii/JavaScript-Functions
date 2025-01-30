85200f4e53c13b2df31ad145a634d2a623d5aee5
Mehdi Jeyrani <mehdijeyrani@gmail.com>
2025-01-30 13:09:28 +0330

    Add example of using reduce() as a higher-order function

--- higher-order-functions.js
+++ higher-order-functions.js
@@ -49,3 +49,9 @@
   const evenNumbers = numbers.filter((num) => num % 2 === 0);
   console.log(evenNumbers); // Output: [ 2, 4, 6 ]
 }
+
+{
+  const numbers = [1, 2, 3, 4];
+  const sum = numbers.reduce((acc, num) => acc + num, 0);
+  console.log(sum); // Output: 10
+}
