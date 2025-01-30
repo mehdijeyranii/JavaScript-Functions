33b765423df27f32b6a6e803809c3252def962c0
Mehdi Jeyrani <mehdijeyrani@gmail.com>
2025-01-30 11:23:26 +0330

    Add named function expression example

--- function-expression.js
+++ function-expression.js
@@ -5,3 +5,11 @@
 
   console.log(greet("Bob")); // Output: Hello, Bob!
 }
+
+{
+  const greet = function sayHello(name) {
+    return "Hello, " + name + "!";
+  };
+
+  console.log(greet("Ali")); // Output: Hello, Ali!
+}
